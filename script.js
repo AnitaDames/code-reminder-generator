d116a149f3851e7c01c965da8579dda79d44e4c3
TeacherAnita <teacheranitadames@gmail.com>
2022-08-25 21:11:03 -0500

    Create Two Variables In script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+//Strings of Array
+const codeReminders = ["Not all syntax have a closing tag.", "Use proper HTML semantics for markups", 
+"Check for misspell, punctuation and inproper syntax error to avoid running into potential bugs", 
+"Select the correct iterator method for a given task.", "MDN provides information that is useful for programming."]
+
+const codeFacts = ["Functions can be passed into other function as parameters.", 
+"Console.log regularly as you write code, can help you to keep track of the progress you are making.",
+"Variable names cannot start with numbers.", 
+"Arrays can store any data types (including strings, numbers, and booleans)."]
+
